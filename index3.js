367d00508610806c45aa1099eb884dd3084893a2
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-19 11:55:05 +0530

    update

--- index3.js
+++ index3.js
@@ -3,11 +3,19 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is running successfully");
+  res.send("Server started successfully");
 });
 
 app.get("/home", (req, res) => {
   res.send("This is the home page");
+});
+
+app.get("/home/page1", (req, res) => {
+  console.log(req.url);
+  console.log(req.method);
+  console.log(req.body);
+  console.log(req.headers.authorization);  
+  res.send("This is page 1");
 });
 
 app.listen(8080, () => {
