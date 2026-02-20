ae6d5942baa2699258d971a9af335314c63fa29b
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-20 12:54:37 +0530

    ok

---
+++ index9.js
@@ -0,0 +1,32 @@
+// import express from 'express';
+// const app = express();
+// app.listen(8080);
+
+// const logger = 
+// app.get('/', (req, res) => {
+//     // res.json({ message: 'Hello World' });
+//     // res.send('Hello World');
+//     res.json({ name: 'John', age: 21});
+// });
+
+
+import express from "express";
+const app = express();
+app.listen(8080);
+
+const logger = (req, res, next) => {
+    
+  req.message = "This is logger function";
+  //   console.log(req.url);
+  next();
+};
+// app.use(logger);
+app.get("/", logger, (req, res) => {
+  //   app.use(logger);
+  console.log(req.message);
+  res.json(req.url);
+});
+app.get("/home", (req, res) => {
+  console.log(req.message);
+  res.json(req.url);
+});
